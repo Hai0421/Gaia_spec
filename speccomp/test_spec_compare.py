768ea83e086a32887a91e3c30f08f78c1781c586
Frank Kiwy <frank.kiwy@ctie.etat.lu>
2023-04-15 11:39:42 +0200

    Revert "Revert "Move templates and scripts to package directory""
    This reverts commit 109f727fb29afe43a4c4ffc3990d4177a4431495.

--- speccomp/test_spec_compare.py
+++ speccomp/test_spec_compare.py
@@ -1,5 +1,6 @@
 from Spec_compare import compare_spectra
 
+
 def test_compare_spectra():
     sources = [58925512487888896, 378026395579692672, 200296663143599104, 1204754033223498624]  # K5, M0, M5, M9
 
