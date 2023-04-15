a9f4c3c8ee7fb78b0e83c3a6b0bf6b602012685e
Frank Kiwy <frank.kiwy@outlook.com>
2023-04-15 10:46:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='Gaia_spec',
       author='Frank Kiwy',
       author_email='frank.kiwy@outlook.com',
       license='MIT',
-      py_modules=['Spec_compare'],
+      packages=['speccomp'],
       install_requires=['numpy', 'astropy', 'matplotlib', 'specutils', 'GaiaXPy'],
       include_package_data=True)
